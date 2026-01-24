b05fb3d725ac78cb6902cbc3bdfb00b9d3c08d5a
Emile Clark-Boman <emile@agribit.com.au>
2026-01-24 23:44:34 +1000

    add bootstrap process

--- flake.nix
+++ flake.nix
@@ -2,8 +2,10 @@
   description = "NixTypes (nt)";
 
   outputs = _: let
-    # nt depends on the mix subsystem for bootstrapping
-    mix = import ./nt/primitives/mix;
+    # nt depends on the mix subsystem for bootstrapping,
+    # we can fake its dependency on this mwahahahah
+    this.util = import ./nt/primitives/util/bootstrap.nix;
+    mix = import ./nt/primitives/mix {inherit this;};
   in
     import ./nt {inherit mix;};
 }
