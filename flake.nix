ed499de65127e4af79b00af633a587f118058f5d
Emile Clark-Boman <emile@agribit.com.au>
2026-01-27 08:34:41 +1000

    provide flake instead of self

--- flake.nix
+++ flake.nix
@@ -1,14 +1,17 @@
 {
   description = "NixTypes (nt)";
 
-  outputs = _: let
+  outputs = inputs: let
     # Step 1: Bootstrap and blast off (*zooommmmm whoooosshhhhh pppppeeeeeeewww*)
     bootstrap = import ./nt/primitives/bootstrap;
     # Step 2: Lie to Mix about its real identity (it's not ready for the truth...)
     mix = import ./nt/mix {this = bootstrap;};
   in
     # Step 3: Actually import NixTypes
-    import ./nt {inherit mix;};
+    import ./nt {
+      inherit mix;
+      flake = inputs.self;
+    };
 
   # Step 4: Like and subscripe!!1!11!!!!!
 }
