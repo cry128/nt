c84d84231bf7f4b008cfc55c266736a323c67914
Emile Clark-Boman <emile@agribit.com.au>
2026-01-28 08:49:21 +1000

    fix nt depends on pipe-operators

--- flake/nix-unit.nix
+++ flake/nix-unit.nix
@@ -5,30 +5,44 @@
   ...
 }: let
   inherit
-    (deps)
-    nixpkgs
-    nix-unit
+    (builtins)
+    concatStringsSep
     ;
 
   inherit
     (this.util)
     forAllSystems
     ;
+
+  inherit
+    (deps)
+    nixpkgs
+    nix-unit
+    ;
+in let
+  enable-experimental = concatStringsSep " " [
+    "flakes"
+    "nix-command"
+    "pipe-operators"
+  ];
 in {
-  checks = forAllSystems (system: {
-    default =
-      nixpkgs.legacyPackages.${system}.runCommand "tests"
-      {
-        nativeBuildInputs = [nix-unit.packages.${system}.default];
-      }
-      ''
-        export HOME="$(realpath .)"
-        # The nix derivation must be able to find all used inputs in the nix-store because it cannot download it during buildTime.
-        nix-unit --eval-store "$HOME" \
-          --extra-experimental-features flakes \
-          --override-input nixpkgs ${nixpkgs} \
-          --flake ${flake}#tests
-        touch $out
-      '';
-  });
+  # XXX: TODO: assert false || throw "TEST: type of flake is \"${builtins.typeOf flake}\""
+  checks = forAllSystems (
+    system: pkgs: {
+      default =
+        pkgs.runCommand "tests"
+        {
+          nativeBuildInputs = [nix-unit.packages.${system}.default];
+        }
+        ''
+          export HOME="$(realpath .)"
+          # The nix derivation must be able to find all used inputs in the nix-store because it cannot download it during buildTime.
+          nix-unit --eval-store "$HOME"                            \
+            --extra-experimental-features "${enable-experimental}" \
+            --override-input nixpkgs ${nixpkgs}                    \
+            --flake ${flake}\#tests
+          touch $out
+        '';
+    }
+  );
 }
